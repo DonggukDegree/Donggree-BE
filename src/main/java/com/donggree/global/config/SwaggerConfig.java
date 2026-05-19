91d02ba10614dfe630b65107700f68ca931ca45f
SeungyeonHa <0727ha@naver.com>
2026-05-19 16:08:51 +0900

    [Feat/#3] Swagger 설정 추가 -OpenAPI JWT Bearer 인증 설정 -프로젝트 정보 및 로컬 서버 설정

---
+++ src/main/java/com/donggree/global/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.donggree.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * Swagger/OpenAPI 설정
+ * - Swagger UI: /swagger-ui.html
+ * - OpenAPI JSON: /v3/api-docs
+ */
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT TOKEN";
+
+    @Bean
+    public OpenAPI donggreeOpenAPI() {
+        return new OpenAPI()
+            .info(apiInfo())
+            .servers(serverList())
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(securityComponents());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Donggree API")
+            .description("PDF 기반 졸업 요건 자동 판정 서비스")
+            .version("0.0.1");
+    }
+
+    private List<Server> serverList() {
+        return List.of(
+            new Server()
+                .url("http://localhost:8080")
+                .description("로컬 개발 서버")
+        );
+    }
+
+    private Components securityComponents() {
+        return new Components()
+            .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                .name(SECURITY_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT"));
+    }
+}
